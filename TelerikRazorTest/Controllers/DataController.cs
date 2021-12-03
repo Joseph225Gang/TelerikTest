49e80e65f61b4de363efc8c9b4bae3e571125528
Joseph.Lai <Joseph.Lai@metaedge.com.tw>
2021-12-03 16:29:31 +0800

    Telerik UI

---
+++ TelerikRazorTest/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikRazorTest.Models;
+
+namespace TelerikRazorTest.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly IDbConnection _conn;
+
+        public DataController(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<auth_Users> list = this._conn.Query<auth_Users>("Select * from auth_Users");
+            return View(list);
+        }
+    }
+}
---
+++ TelerikRazorTest/Models/auth_Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelerikRazorTest.Models
+{
+    public partial class auth_Users
+    {
+        public int UserId { get; set; }
+        public Nullable<int> AffiliateId { get; set; }
+        public string UserCode { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Nullable<short> SignOnRetryLimit { get; set; }
+        public Nullable<short> SignOnErrorCnt { get; set; }
+        public Nullable<bool> Suspended { get; set; }
+        public Nullable<System.DateTime> SuspendedDate { get; set; }
+        public Nullable<System.DateTime> LastPasswordChangedDate { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public string Domain { get; set; }
+        public string Lst_Maint_Usr { get; set; }
+        public Nullable<System.DateTime> Lst_Maint_Dt { get; set; }
+    }
+}
